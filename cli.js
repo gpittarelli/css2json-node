35293df810f282e505fd68dba55cdfa8af553dbf
George Pittarelli <g@gjp.cc>
2019-06-18 17:15:13 -0700

    Clean up raws/source in CLI output

--- cli.js
+++ cli.js
@@ -8,6 +8,15 @@ process.stdin.on('data', function (chunk) {
 
 process.stdin.on('end', function () {
   postcss2json(data).forEach(function (rule) {
+    delete rule.raws;
+    delete rule.source;
+    if (rule.nodes) {
+      rule.nodes.forEach(n => {
+        delete n.raws;
+        delete n.source;
+      });
+    }
+
     process.stdout.write(JSON.stringify(rule) + '\n');
   });
 });
